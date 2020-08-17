21e409a87caa79f82d38e7a5ab436892059ee108
Chris Colvard <chris.colvard@gmail.com>
2020-08-17 14:52:14 -0400

    Fill out generators for everything needed to setup DOI minting in the DOI tab
    Also exclude any generator template files from coverage data.

--- lib/generators/hyrax/doi/add_to_work_type_generator.rb
+++ lib/generators/hyrax/doi/add_to_work_type_generator.rb
@@ -18,11 +18,13 @@ module Hyrax
       # Namespaces passed as the argument will still appear in class_path
       class_option :skip_namespace, default: true
 
-      desc "Add DOI support to given work type"
+      # DataCite-specific support
+      class_option :datacite, type: :boolean, default: false, desc: "Add DataCite-specific behavior."
 
+      desc "Add DOI support to given work type"
       def inject_into_model
-        # For some reason I had to use self.destination_root here to get all contexts to work (calling from hyrax app, calling from this engine to test app, rspec tests)
         # rubocop:disable Style/RedundantSelf
+        # For some reason I had to use self.destination_root here to get all contexts to work (calling from hyrax app, calling from this engine to test app, rspec tests)
         self.destination_root = Rails.root if self.destination_root.blank? || self.destination_root == Hyrax::DOI::Engine.root.to_s
         model_file = File.join(self.destination_root, 'app', 'models', *class_path, "#{file_name}.rb")
         # rubocop:enable Style/RedundantSelf
@@ -31,6 +33,39 @@ module Hyrax
           "\n" \
           "  # Adds behaviors for hyrax-doi plugin.\n" \
           "  include Hyrax::DOI::DOIBehavior"
+        end
+
+        return unless options[:datacite]
+
+        # DataCite specific behavior
+        insert_into_file model_file, after: 'include Hyrax::DOI::DOIBehavior' do
+          "\n" \
+          "  # Adds behaviors for DataCite DOIs via hyrax-doi plugin.\n" \
+          "  include Hyrax::DOI::DataCiteDOIBehavior"
+        end
+      end
+
+      desc "Add DOI support to given work type form"
+      def inject_into_form
+        # rubocop:disable Style/RedundantSelf
+        # For some reason I had to use self.destination_root here to get all contexts to work (calling from hyrax app, calling from this engine to test app, rspec tests)
+        self.destination_root = Rails.root if self.destination_root.blank? || self.destination_root == Hyrax::DOI::Engine.root.to_s
+        form_file = File.join(self.destination_root, 'app', 'forms', 'hyrax', *class_path, "#{file_name}_form.rb")
+        # rubocop:enable Style/RedundantSelf
+
+        insert_into_file form_file, after: 'Hyrax::Forms::WorkForm' do
+          "\n" \
+          "    # Adds behaviors for hyrax-doi plugin.\n" \
+          "    include Hyrax::DOI::DOIFormBehavior"
+        end
+
+        return unless options[:datacite]
+
+        # DataCite specific behavior
+        insert_into_file form_file, after: 'include Hyrax::DOI::DOIFormBehavior' do
+          "\n" \
+          "    # Adds behaviors for DataCite DOIs via hyrax-doi plugin.\n" \
+          "    include Hyrax::DOI::DataCiteDOIFormBehavior"
         end
       end
     end
--- spec/generators/add_to_work_type_generator_spec.rb
+++ spec/generators/add_to_work_type_generator_spec.rb
@@ -3,10 +3,6 @@ require 'rails_helper'
 # Generators are not automatically loaded by Rails
 require 'generators/hyrax/doi/add_to_work_type_generator'
 
-def model_path
-  File.join('app', 'models', "#{klass.underscore}.rb")
-end
-
 describe Hyrax::DOI::AddToWorkTypeGenerator, type: :generator do
   # Tell the generator where to put its output (what it thinks of as Rails.root)
   destination Hyrax::DOI::Engine.root.join("tmp", "generator_testing")
@@ -15,23 +11,83 @@ describe Hyrax::DOI::AddToWorkTypeGenerator, type: :generator do
     prepare_destination
 
     # Setup work type files in generator testing destination root dir
+    # Model
     FileUtils.mkdir_p destination_root.join(File.dirname(model_path))
     FileUtils.cp Rails.root.join(model_path), destination_root.join(model_path)
+    # Helper
+    FileUtils.mkdir_p destination_root.join(File.dirname(form_path))
+    FileUtils.cp Rails.root.join(form_path), destination_root.join(form_path)
   end
 
   let(:klass) { 'GenericWork' }
+  let(:model_path) { File.join('app', 'models', "#{klass.underscore}.rb") }
+  let(:form_path) { File.join('app', 'forms', 'hyrax', "#{klass.underscore}_form.rb") }
 
-  it 'adds behavior module to model class' do
-    run_generator [klass]
-    expect(file(model_path)).to contain('include Hyrax::DOI::DOIBehavior')
-  end
-
-  context 'with a namespaced model class' do
-    let(:klass) { 'NamespacedWorks::NestedWork' }
-
-    it 'adds behavior module tod model class' do
+  describe 'inject_into_model' do
+    it 'adds behavior module to model class' do
       run_generator [klass]
       expect(file(model_path)).to contain('include Hyrax::DOI::DOIBehavior')
+    end
+
+    context 'with a namespaced model class' do
+      let(:klass) { 'NamespacedWorks::NestedWork' }
+
+      it 'adds behavior module to model class' do
+        run_generator [klass]
+        expect(file(model_path)).to contain('include Hyrax::DOI::DOIBehavior')
+      end
+    end
+
+    context 'datacite enabled' do
+      it 'adds behavior module to model class' do
+        run_generator [klass, "--datacite"]
+        expect(file(model_path)).to contain('include Hyrax::DOI::DOIBehavior')
+        expect(file(model_path)).to contain('include Hyrax::DOI::DataCiteDOIBehavior')
+      end
+
+      context 'with a namespaced model class' do
+        let(:klass) { 'NamespacedWorks::NestedWork' }
+
+        it 'adds behavior module to model class' do
+          run_generator [klass, "--datacite"]
+          expect(file(model_path)).to contain('include Hyrax::DOI::DOIBehavior')
+          expect(file(model_path)).to contain('include Hyrax::DOI::DataCiteDOIBehavior')
+        end
+      end
+    end
+  end
+
+  describe 'inject_into_form' do
+    it 'adds behavior module to form class' do
+      run_generator [klass]
+      expect(file(form_path)).to contain('include Hyrax::DOI::DOIFormBehavior')
+    end
+
+    context 'with a namespaced model class' do
+      let(:klass) { 'NamespacedWorks::NestedWork' }
+
+      it 'adds behavior module to form class' do
+        run_generator [klass]
+        expect(file(form_path)).to contain('include Hyrax::DOI::DOIFormBehavior')
+      end
+    end
+
+    context 'datacite enabled' do
+      it 'adds behavior module to form class' do
+        run_generator [klass, "--datacite"]
+        expect(file(form_path)).to contain('include Hyrax::DOI::DOIFormBehavior')
+        expect(file(form_path)).to contain('include Hyrax::DOI::DataCiteDOIFormBehavior')
+      end
+
+      context 'with a namespaced model class' do
+        let(:klass) { 'NamespacedWorks::NestedWork' }
+
+        it 'adds behavior module to form class' do
+          run_generator [klass, "--datacite"]
+          expect(file(form_path)).to contain('include Hyrax::DOI::DOIFormBehavior')
+          expect(file(form_path)).to contain('include Hyrax::DOI::DataCiteDOIFormBehavior')
+        end
+      end
     end
   end
 end
