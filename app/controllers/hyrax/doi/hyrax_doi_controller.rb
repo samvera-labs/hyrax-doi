df260ad8e3811d595c2b45f030d4eb6be1860df8
LaRita Robinson <larita@scientist.com>
2023-10-20 21:32:09 -0400

    😱 Attempt to backport from British library
    ref https://gitlab.com/notch8/britishlibrary/-/merge_requests/244?commit_id=0bbd85583b8cc0246a0f294dfcccddb5603d285d
    
    Code was put into BL to make the doi setup less complicated. This
    attempts to grab the relevant code.

--- app/controllers/hyrax/doi/hyrax_doi_controller.rb
+++ app/controllers/hyrax/doi/hyrax_doi_controller.rb
@@ -25,18 +25,6 @@ module Hyrax
         end
       end
 
-      def autofill
-        doi = params['doi']
-
-        respond_to do |format|
-          format.js { render js: autofill_js(doi), status: :ok }
-        end
-      rescue Hyrax::DOI::NotFoundError => e
-        respond_to do |format|
-          format.js { render plain: e.message, status: :internal_server_error }
-        end
-      end
-
       private
 
       def check_authorization
@@ -52,10 +40,6 @@ module Hyrax
         Hyrax::Identifier::Registrar.for(:datacite, {})
       end
 
-      def use_sandbox
-        !doi_registrar.mode.equal?(:production)
-      end
-
       def field_selector(attribute_name)
         ".#{params[:curation_concern]}_#{attribute_name}"
       end
@@ -64,35 +48,52 @@ module Hyrax
         params[:attribute] || "doi"
       end
 
-      def hyrax_work_from_doi(doi)
-        # TODO: generalize this
-        meta = Bolognese::Metadata.new(input: doi,
-                                       sandbox: use_sandbox)
-        # Check that a record was actually loaded
-        raise Hyrax::DOI::NotFoundError, "DOI (#{doi}) could not be found." if meta.blank? || meta.doi.blank?
-        meta.types["hyrax"] = params['curation_concern'].camelize
-        meta.hyrax_work
+
+      # TODO: make this configurable
+      def js_fields
+        %w(title)
       end
 
       # TODO: Move this out to a partial that gets rendered?
-      def autofill_js(doi)
-        # TODO: Need to wipe old data or is this just supplemental?
-        js = hyrax_work_from_doi(doi).attributes.collect { |k, v| autofill_field(k, v) }.reject(&:blank?).join("\n")
-        js << "document.location = '#metadata';"
-      end
-
-      # TODO: Move this out to a partial that gets rendered?
+      # rubocop:disable Metrics/AbcSize
+      # rubocop:disable Metrics/MethodLength
       def autofill_field(attribute_name, value)
         js = []
         # TODO: add error handling in the JS so an error doesn't leave the autofilling incomplete
+        # rubocop:disable Lint/UselessAssignment
+        position = value if attribute_name == 'funder_position'
+        # rubocop:enable Lint/UselessAssignment
+        js << "  doi_button_var = document.querySelectorAll('#{field_selector(attribute_name)} button.add');"
         Array(value).each_with_index do |v, index|
           # Is this the right way to do this?
           # Need to be smarter to see if all repeated fields are filled before trying to create a new one by clicking?
-          js << "document.querySelectorAll('#{field_selector(attribute_name)} button.add')[0].click();" unless index.zero?
-          js << "document.querySelectorAll('#{field_selector(attribute_name)} .form-control')[#{index}].value = '#{helpers.escape_javascript(v)}';"
+          unless index.zero?
+            js << "if(doi_button_var[0] != undefined) {"
+            js << "document.querySelectorAll('#{field_selector(attribute_name)} button.add')[0].click();"
+            js << "}"
+          end
+          if attribute_name.include?('date')
+            dates = v.split('-')
+            js << "if(document.querySelectorAll('#{field_selector(attribute_name)} .form-control')[#{index}] != undefined) {"
+            js << "  document.querySelectorAll('#{field_selector(attribute_name)} .form-control#date_year')[#{index}].value = '#{helpers.escape_javascript(dates[0].to_i.to_s)}';" if dates[0]
+            js << "  document.querySelectorAll('#{field_selector(attribute_name)} .form-control#date_month')[#{index}].value = '#{helpers.escape_javascript(dates[1].to_i.to_s)}';" if dates[1]
+            js << "  document.querySelectorAll('#{field_selector(attribute_name)} .form-control#date_day')[#{index}].value = '#{helpers.escape_javascript(dates[2].to_i.to_s)}';" if dates[2]
+            js << "}"
+            next
+          end
+
+          js << "$.when(document.querySelectorAll('a.add_#{attribute_name.split('_')&.first}')[#{index}].click()).then(function() {" if js_fields.include?(attribute_name) && index < Array(value).length - 1
+          js << "  doi_form_var = document.querySelectorAll('#{field_selector(attribute_name)} .form-control, #{field_selector(attribute_name)} .select-control');"
+          js << "  if(doi_form_var[#{index}] != undefined) {"
+          js << "    document.querySelectorAll('#{field_selector(attribute_name)} .form-control, #{field_selector(attribute_name)} .select-control')[#{index}].value = '#{helpers.escape_javascript(v)}';"
+          js << "    $(document.querySelectorAll('#{field_selector(attribute_name)} .form-control, #{field_selector(attribute_name)} .select-control')[#{index}]).change();" if js_fields.include?(attribute_name)
+          js << "  }"
+          js << "})" if ajs_fields.include?(attribute_name) && index < Array(value).length - 1
         end
         js.reject(&:blank?).join("\n")
       end
+      # rubocop:enable Metrics/AbcSize
+      # rubocop:enable Metrics/MethodLength      
 
       # Override of Hyrax method (See https://github.com/samvera/hyrax/pull/4495)
       # render a json response for +response_type+
